d4a1d6d2f4c0db164faaa5e442f540d39a3d1a5e
Dave Gee <dgee519@gmail.com>
2024-01-26 01:52:33 +0000

    W3D2

---
+++ demo2.rb
@@ -0,0 +1,28 @@
+def yellow_page (phone_string) 
+    puts phone_string
+    name_string = ""
+    phone_string.downcase.each_char do |letter|
+        if letter = "a" || letter = "b" || letter = "c"
+            name_string += '2'
+        elsif letter = "d" || letter = "e" || letter = "f"
+            name_string += '2'
+        elsif letter = "g" || letter = "h" || letter = "i"
+            name_string += '2'
+        elsif
+        if letter = "j" || letter = "k" || letter = "l"
+            name_string += '2'
+        end
+        if letter = "m" || letter = "n" || letter = "o"
+            name_string += '2'
+        end
+        if letter = "p" || letter = "q" || letter = "r"
+            name_string += '2'
+        end
+        if letter = "s" || letter = "t" || letter = "u"
+            name_string += '2'
+        end
+    end
+end
+
+
+yellow_page("test")
