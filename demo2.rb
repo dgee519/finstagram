7cfd13caf10cc20b504cb92ae80f7e4e62115992
Dave Gee <dgee519@gmail.com>
2024-01-30 00:42:00 +0000

    W4D1

--- demo2.rb
+++ demo2.rb
@@ -1,28 +1,45 @@
 def yellow_page (phone_string) 
     puts phone_string
-    name_string = ""
-    phone_string.downcase.each_char do |letter|
-        if letter = "a" || letter = "b" || letter = "c"
-            name_string += '2'
-        elsif letter = "d" || letter = "e" || letter = "f"
-            name_string += '2'
-        elsif letter = "g" || letter = "h" || letter = "i"
-            name_string += '2'
-        elsif
-        if letter = "j" || letter = "k" || letter = "l"
-            name_string += '2'
+bundle 
+        puts "arg is 10 characters"
+        name_string = ""
+        phone_string.downcase.each_char do |letter|
+            if letter == "a" || letter == "b" || letter == "c"
+                name_string += '2'
+            elsif letter == "d" || letter == "e" || letter == "f"
+                name_string += '3'
+            elsif letter == "g" || letter == "h" || letter == "i"
+                name_string += '4'
+            elsif letter == "j" || letter == "k" || letter == "l"
+                name_string += '5'
+            elsif letter == "m" || letter == "n" || letter == "o"
+                name_string += '6'
+            elsif letter == "p" || letter == "q" || letter == "r" || letter == "s"
+                name_string += '7'
+            elsif letter == "t" || letter == "u" || letter == "v"
+                name_string += '8'
+            elsif letter == "w" || letter == "x" || letter == "y" || letter == "z"
+                name_string += '9'
+            else
+                name_string += letter
+            end
         end
-        if letter = "m" || letter = "n" || letter = "o"
-            name_string += '2'
-        end
-        if letter = "p" || letter = "q" || letter = "r"
-            name_string += '2'
-        end
-        if letter = "s" || letter = "t" || letter = "u"
-            name_string += '2'
-        end
+        puts name_string
+        return true
     end
+    
 end
 
 
-yellow_page("test")
+test_val = yellow_page("testingabc")
+if test_val == true
+    puts "return value true"
+else
+    puts "return value false"
+end
+test_val = yellow_page("testnum2")
+if test_val == true
+    puts "return value true"
+else
+    puts "return value false"
+end
